784bcf54a05fc768b4fde7c1a4ced119ba47c4da
danielesala <danysala8@gmail.com>
2024-10-04 10:40:11 +0200

    Update BaseEntity.java

--- src/main/java/it/eforhum/noleggi/entity/BaseEntity.java
+++ src/main/java/it/eforhum/noleggi/entity/BaseEntity.java
@@ -1,5 +1,40 @@
 package it.eforhum.noleggi.entity;
 
-public class BaseEntity {
+import java.time.LocalDate;
 
+public abstract class BaseEntity {
+	
+	protected int id;
+	protected String codice;
+	protected String titolo;
+	protected LocalDate dataInizio;
+	protected LocalDate dataFine;
+	
+	public double getPrezzo() {
+		return 0;
+	}
 }
+
+//ESERCITAZIONE IN AUTONOMIA/GRUPPO
+//Scrivere un'applicazione standalone Java, in grado di simulare un sistema di noleggio giochi e film.
+//
+//Sia i giochi che i film hanno le seguenti caratteristiche
+//- identificativo numerico univoco (es. 1)
+//- codice alfanumerico (es. PS5-GT7)
+//- titolo alfanumerico (es. Mamma ho perso l'aereo)
+//- data inizio noleggio (es. 10/12/2020)
+//- data fine noleggio (es. 12/12/2020)
+//- prezzo noleggio calcolato dal sistema
+//
+//I giochi hanno come attributi non comuni
+//- console di riferimento (valori ammissibili PC/PS4/PS5/XBOX-ONE-S/XBOX-ONE-X)
+//
+//I film hanno come attributi non comuni
+//- genere (valori ammissibili ANIMAZIONE/AZIONE/COMMEDIA/DRAMMATICO/FAMIGLIA)
+
+//Il prezzo di noleggio e' cosi' calcolato
+//giochi - 1 * numero di giorni calcolati come differenza tra la data fine e la data inizio
+//film - 2.5 * numero di giorni calcolati come differenza tra la data fine e la data inizio
+
+//Implementare i metodi per calcolare il noleggio sia per i giochi che per i film
+//implementare anche i metodi per aggiungere togliere ecc....
