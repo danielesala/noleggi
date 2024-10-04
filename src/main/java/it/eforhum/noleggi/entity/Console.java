3810c54b7599cf39a43cb192eb297c775766155d
Neander Alves <115661095+NeanderAlves05@users.noreply.github.com>
2024-10-04 11:00:04 +0200

    videogioco e console
    fsdknfk

---
+++ src/main/java/it/eforhum/noleggi/entity/Console.java
@@ -0,0 +1,9 @@
+package it.eforhum.noleggi.entity;
+
+public enum Console {
+	PC,
+	PS4,
+	PS5,
+	XBOX_ONE_S,
+	XBOX_ONE_X
+}	
--- src/main/java/it/eforhum/noleggi/entity/Videogioco.java
+++ src/main/java/it/eforhum/noleggi/entity/Videogioco.java
@@ -1,5 +1,23 @@
 package it.eforhum.noleggi.entity;
 
-public class Videogioco extends BaseEntity{
+import java.time.LocalDate;
+import java.time.Period;
 
+public class Videogioco extends BaseEntity{
+	Console console;
+
+	public Videogioco(int id, String codice, String titolo, LocalDate dataInizio, LocalDate dataFine) {
+		this.id = id;
+		this.codice = codice;
+		this.titolo = titolo;
+		this.dataInizio = dataInizio;
+		this.dataFine = dataFine;
+	}
+
+	@Override
+	public double getPrezzo() {
+		long period=super.dataFine.toEpochDay()-super.dataInizio.toEpochDay();
+		return 1*(period);
+	}
+	
 }
