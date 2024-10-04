ab7f12311683c99ee2ff1b46a6cbec8b708fc5e5
Neander Alves <115661095+NeanderAlves05@users.noreply.github.com>
2024-10-04 11:46:33 +0200

    dao videogiochi
    asdf

--- src/main/java/it/eforhum/noleggi/dao/VideogiocoDao.java
+++ src/main/java/it/eforhum/noleggi/dao/VideogiocoDao.java
@@ -1,15 +1,22 @@
 package it.eforhum.noleggi.dao;
 
+import java.util.List;
+import java.util.Optional;
+
 import it.eforhum.noleggi.entity.Videogioco;
 
 public interface VideogiocoDao {
 	
 	boolean save(Videogioco newVideogioco);
 	
-	boolean update(long id,Videogioco videogioco);
+	boolean update(int id,Videogioco videogioco);
 	
 	boolean delete(Videogioco videogioco);
 	
+	boolean deleteById(int id);
 	
+	Optional<Videogioco> findById(int id);
+	
+	List<Videogioco> findAll();
 	
 }
--- src/main/java/it/eforhum/noleggi/dao/VideogiocoRamDao.java
+++ src/main/java/it/eforhum/noleggi/dao/VideogiocoRamDao.java
@@ -1,33 +1,61 @@
 package it.eforhum.noleggi.dao;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import it.eforhum.noleggi.entity.Videogioco;
 
 public class VideogiocoRamDao implements VideogiocoDao{
-	Map<Long, Videogioco> map=new HashMap<>();
+	Map<Integer, Videogioco> map=new HashMap<>();
 	
 	@Override
 	public boolean save(Videogioco newVideogioco) {
-		map.containsValue(newVideogioco);
-		
+		if(map.containsValue(newVideogioco)) {
+			return false;//gioco già presente
+		}
+		map.put(newVideogioco.getId(), newVideogioco);
 		return true;
-		
 	}
 
 	@Override
 	public boolean delete(Videogioco videogioco) {
-		// TODO Auto-generated method stub
-		return false;
+		if(!map.containsValue(videogioco)) {
+			return false;//videogioco non presente
+		}
+		map.remove(videogioco.getId(), videogioco);
+		return true;
 	}
 
 	@Override
-	public boolean update(long id, Videogioco videogioco) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean update(int id, Videogioco videogioco) {
+		if(findById(id).isEmpty()) {//key not present
+			return false;
+		}
+		map.replace(id, map.get(id), videogioco);
+		return true;
 	}
+
+	@Override
+	public boolean deleteById(int id) {
+		if(findById(id).isEmpty()) {//key not present
+			return false;
+		}
+		map.remove(id);
+		return true;
+	}
+	
+	@Override
+	public Optional<Videogioco> findById(int id) {
+		return Optional.of(map.get(id));
+	}
+
+	@Override
+	public List<Videogioco> findAll() {
+		return (List<Videogioco>) map.values();
+	}
+
+	
 	
 }
